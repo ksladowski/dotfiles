687993a7439538dcacdb937dd0753b9a6c08ac07
Kevin Sladowski <ksladowski@mailbox.org>
2026-05-10 12:51:13 -0500

    environment.d

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,9 +3,6 @@ if test -e $cachyos_config
     source $cachyos_config
 end
 
-set SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-set SSL_CERT_DIR $HOME/.aspnet/dev-certs/trust:/etc/ssl/certs
-
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
